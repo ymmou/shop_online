21122ea47f4c4bd73b11dbd045af2d4beae086d5
yanglvzhi <1952426800@qq.com>
2023-11-09 21:31:59 +0800

    Initial commit

---
+++ src/main/java/com/soft2242/shop/ShopOnlineApplication.java
@@ -0,0 +1,25 @@
+package com.soft2242.shop;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.fill.Column;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.util.*;
+@SpringBootApplication
+@MapperScan("com.soft2242.shop.mapper")
+public class ShopOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOnlineApplication.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/soft2242/shop/common/constant/Constant.java
@@ -0,0 +1,11 @@
+package com.soft2242.shop.common.constant;
+
+public interface Constant {
+    //    创建时间
+    String CREATE_TIME = "createTime";
+    //    更新时间
+    String UPDATE_TIME = "updateTime";
+    //   逻辑删除
+    String DELETE_FLAG = "deleteFlag";
+
+}
